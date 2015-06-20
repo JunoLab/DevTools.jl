1eee9e813351f303f4dbc0bc522ae8ec33cf43b9
Mike Innes <mike.j.innes@gmail.com>
2015-06-21 00:49:18 +0100

    new exports

--- src/DevTools.jl
+++ src/DevTools.jl
@@ -2,7 +2,7 @@ module DevTools
 
 using Blink, Media, Lazy
 
-export BlinkDisplay, pin, top
+export BlinkDisplay, pin, top, docs, profiler
 
 include("display/BlinkDisplay.jl")
 using .BlinkDisplay
@@ -10,6 +10,6 @@ include("profile/profile.jl")
 include("codemirror.jl")
 include("collab.jl")
 
-profile() = ProfileView.fetch()
+profiler() = ProfileView.fetch()
 
 end # module
