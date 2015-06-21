2b5684723d5152b3efd92f41cde8e057732d082f
Mike Innes <mike.j.innes@gmail.com>
2015-06-21 01:32:46 +0100

    firepad updates

--- src/collab.jl
+++ src/collab.jl
@@ -2,8 +2,8 @@ using Blink, Mux
 
 function collab(p)
   loadeditor(p, ver = "4.3.0")
-  load!(p, "https://cdn.firebase.com/js/client/2.0.2/firebase.js")
-  load!(p, "http://www.firepad.io/examples/firepad.js")
+  load!(p, "https://cdn.firebase.com/js/client/2.2.4/firebase.js")
+  load!(p, "https://cdn.firebase.com/libs/firepad/1.1.0/firepad.min.js")
   @js_ p begin
     @var firebase = @new Firebase("crackling-heat-4207.firebaseio.com")
     @var firepad = Firepad.fromCodeMirror(firebase, cm)
